575dcabf7f14816c8c56129e3e3f2377d3b009bf
Mandar Namdas <mandar.namdas@unico.com.au>
2016-04-28 10:13:27 +0530

    Add Rest controller.

---
+++ SpringHibernateDemo/src/main/java/com/nihilent/springmvc/configuration/MVCConfiguration.java
@@ -0,0 +1,12 @@
+package com.nihilent.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.nihilent.springmvc")
+public class MVCConfiguration {
+
+}
---
+++ SpringHibernateDemo/src/main/java/com/nihilent/springmvc/configuration/MVCInitializer.java
@@ -0,0 +1,27 @@
+package com.nihilent.springmvc.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MVCInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        // TODO Auto-generated method stub
+        return new String[] {
+            "/"
+        };
+    }
+
+}
