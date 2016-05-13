fe2f9d6dd33e0838748cac197faff3bdef06e907
Mandar Namdas <mandar.namdas@unico.com.au>
2016-05-13 14:00:41 +0530

    SpringRESTEasy project.

---
+++ SpringRESTEasy/src/main/java/com/nihilent/service/MyService.java
@@ -0,0 +1,11 @@
+package com.nihilent.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+    public void process() {
+        System.out.println("Processing start...");
+    }
+}
